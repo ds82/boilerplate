ab6c2003d3104d56a32ff2250f17a96150cfdcf6
Dennis Sänger <mail@dennis.io>
2015-08-27 09:41:01 +0200

    fix(gulp): watch modules folder

--- client/gulp/tasks/watch.js
+++ client/gulp/tasks/watch.js
@@ -4,7 +4,11 @@ var gulp = require('gulp');
 var livereload = require('./livereload');
 
 gulp.task('watch', ['build', 'lr-server'], function() {
-  gulp.watch(['./app/js/**/*.js', '!./app/js/common.js'], ['browserify:bundle']);
+  gulp.watch([
+    './app/js/**/*.js',
+    './app/modules/**/*.js',
+    '!./app/js/common.js'
+  ], ['browserify:bundle']);
   gulp.watch('./app/js/common.js', ['browserify:common']);
   gulp.watch('./app/**/*.html', livereload.reload);
   gulp.watch('./app/less/*.less', ['less']);
