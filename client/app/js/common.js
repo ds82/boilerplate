3c5970c942c525d5b0ad46cb187da815cdcb5246
Dennis Sänger <mail@dennis.io>
2015-08-27 09:43:00 +0200

    chore(package): insert common deps at fixed position
    The common.js file is now not auto-generated from scratch, but the runtime
    deps are automatically inserted between two javascript comments:
    
    // REQUIRE_START
    // everything between these comments is replaced with
    // require calls to runtime deps
    // REQUIRE_ENDE

---
+++ client/app/js/common.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// ugly bootstrap patch
+window.jQuery = require('jquery');
+
+// REQUIRE_START
+require('angular');
+require('angular-strap');
+require('angular-ui-router');
+require('bootstrap');
+require('jquery');
+require('ng.config');
+// REQUIRE_END
--- client/gulp/tasks/browserify.js
+++ client/gulp/tasks/browserify.js
@@ -14,6 +14,8 @@ var livereload = require('./livereload');
 const PATH_TO_COMMON = path.resolve(__dirname, '../../app/js/common.js');
 const COMMON_DEPS = Object.keys(pck.dependencies);
 
+const REQUIRE_REGEXP = /\/\/ REQUIRE_START[\s\S]*\/\/ REQUIRE_END\n?/;
+
 gulp.task('browserify', seq(
   ['browserify:bundle', 'browserify:before:common'],
   'browserify:common'
@@ -36,11 +38,33 @@ gulp.task('browserify:bundle', function() {
 });
 
 gulp.task('browserify:before:common', (done) => {
-  var fileContents = `'use strict';
-${makeRequireCalls(COMMON_DEPS)}
-`
-  fs.writeFile(PATH_TO_COMMON, fileContents, done);
+  var fileContents = `// REQUIRE_START
+${makeRequireCalls(COMMON_DEPS)}// REQUIRE_END
+`;
+  readFile(PATH_TO_COMMON)
+    .then(data => data.replace(REQUIRE_REGEXP, fileContents))
+    .then(data => writeFile(PATH_TO_COMMON, data))
+    .then(() => done())
+    .catch(err => { console.log('error', err); done(); });
 });
+
+function readFile(file) {
+  console.log('readFile');
+  return new Promise(function(resolve, reject) {
+    fs.readFile(file, (err, contents) => {
+      console.log(err, contents);
+      return (err) ? reject(err) : resolve(contents.toString());
+    });
+  });
+}
+
+function writeFile(file, data) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(file, data, (err) => {
+      return (err) ? reject(err) : resolve(data);
+    })
+  });
+}
 
 function makeRequireCalls(deps = []) {
   var code = '';
@@ -50,8 +74,12 @@ function makeRequireCalls(deps = []) {
 
 gulp.task('browserify:common', function() {
   var common = new Brify();
-  return common
-    .add('./app/js/common')
+  var stream = common
+    .add('./app/js/common');
+
+  COMMON_DEPS.forEach(dep => stream.require(dep));
+
+  return stream
     .bundle()
     .pipe(source('common.js'))
     .pipe(buffer())
